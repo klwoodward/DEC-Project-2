13e679eaeb10ddb2c1145edf09b80834bf6419d9
klwoodward <klwoodward99@yahoo.com>
2026-05-09 22:32:23 -0700

    readme instructions and dbt updates. most work has been trial and error through cloud services

---
+++ transform/dbt_transforms/models/marts/composite/data_center_atlas_obt.sql
@@ -0,0 +1,50 @@
+--{{ config(materialized='view') }}
+
+
+-- collapse data so that grain of table is a datacenter, not datacenter/county combo
+WITH ranked AS (
+    SELECT
+        *,
+        ROW_NUMBER() OVER ( -- we will grab the first two regions of each data center, ordered alphabetically; if a data center has more than two regions, the rest will be ignored
+            PARTITION BY id
+            ORDER BY county_id
+        ) AS county_rank
+    FROM {{ ref('data_center_atlas') }}
+),
+primary_county AS (
+    SELECT *
+    FROM ranked
+    WHERE county_rank = 1
+),
+secondary_county AS (
+
+    SELECT *
+    FROM ranked
+    WHERE county_rank = 2
+)
+SELECT
+    p.id,
+    p.county,
+    p.county_id,
+    p.state,
+    p.state_abb,
+    p.state_id,
+    s.county AS second_county,
+    s.county_id AS second_county_id,
+    s.state AS second_state,
+    s.state_abb AS second_state_abb,
+    s.state_id AS second_state_id,
+    p.ref,
+    p.operator,
+    p.name,
+    p.sqft,
+    p.lon,
+    p.lat,
+    p.type,
+    p.service_start_date,
+    TO_DATE(p._ab_cdc_deleted_at) AS decommission_date,
+    p._ab_cdc_updated_at AS source_updated_at
+FROM 
+    primary_county p
+    LEFT JOIN secondary_county s
+        ON p.id = s.id
